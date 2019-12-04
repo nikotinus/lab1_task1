5c5a05e403bea9c593f5af21220891d8455a1625
Alexander Serkin <serkin2005@mail.ru>
2019-12-04 23:32:58 +0300

    add method post

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import View
+from django.http import HttpResponse
 # Create your views here.
 
 
@@ -7,27 +8,75 @@ class HomePageView(View):
     def get(self, request):
         return render(request, 'index.html')
 
+    def post(self, request):
+        html = "<html><body><h1>I'am working!</h1>"
+        html += "<a>User filled the form:</a><br>"
+        for key, value in request.POST.items():
+            html += f"Field - {key}, value - {value}<br>"
+        html += '</body></html>'
+        return HttpResponse(html)
+
 
 class AboutPageView(View):
     def get(self, request):
         return render(request, 'about-us.html')
+
+    def post(self, request):
+        html = "<html><body><h1>I'am working!</h1>"
+        html += "<a>User filled the form:</a><br>"
+        for key, value in request.POST.items():
+            html += f"Field - {key}, value - {value}<br>"
+        html += '</body></html>'
+        return HttpResponse(html)
 
 
 class ContactPageView(View):
     def get(self, request):
         return render(request, 'contact.html')
 
+    def post(self, request):
+        html = "<html><body><h1>I'am working!</h1>"
+        html += "<a>User filled the form:</a><br>"
+        for key, value in request.POST.items():
+            html += f"Field - {key}, value - {value}<br>"
+        html += '</body></html>'
+        return HttpResponse(html)
+
 
 class LoansPageView(View):
     def get(self, request):
         return render(request, 'loans.html')
+
+    def post(self, request):
+        html = "<html><body><h1>I'am working!</h1>"
+        html += "<a>User filled the form:</a><br>"
+        for key, value in request.POST.items():
+            html += f"Field - {key}, value - {value}<br>"
+        html += '</body></html>'
+        return HttpResponse(html)
 
 
 class ElementPageView(View):
     def get(self, request):
         return render(request, 'elements.html')
 
+    def post(self, request):
+        html = "<html><body><h1>I'am working!</h1>"
+        html += "<a>User filled the form:</a><br>"
+        for key, value in request.POST.items():
+            html += f"Field - {key}, value - {value}<br>"
+        html += '</body></html>'
+        return HttpResponse(html)
+
 
 class NewsPageView(View):
     def get(self, request):
         return render(request, 'news.html')
+
+    def post(self, request):
+        html = "<html><body><h1>I'am working!</h1>"
+        html += "<a>User filled the form:</a><br>"
+        for key, value in request.POST.items():
+            html += f"Field - {key}, value - {value}<br>"
+        html += '</body></html>'
+        return HttpResponse(html)
