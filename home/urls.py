3c8b946488f2d41f447f600449a05cd826f15e00
Alexander Serkin <serkin2005@mail.ru>
2019-12-04 22:11:38 +0300

    add 3 more pages

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, AboutPageView, ContactPageView, LoansPageView
 
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('loans.html', LoansPageView.as_view(), name='loans'),
+    path('contact.html', ContactPageView.as_view(), name='contact'),
+    path('index.html', HomePageView.as_view(), name='index'),
+    path('about-us.html', AboutPageView.as_view(), name='about'),
 ]
--- home/views.py
+++ home/views.py
@@ -6,3 +6,18 @@ from django.views.generic import View
 class HomePageView(View):
     def get(self, request):
         return render(request, 'index.html')
+
+
+class AboutPageView(View):
+    def get(self, request):
+        return render(request, 'about-us.html')
+
+
+class ContactPageView(View):
+    def get(self, request):
+        return render(request, 'contact.html')
+
+
+class LoansPageView(View):
+    def get(self, request):
+        return render(request, 'loans.html')
